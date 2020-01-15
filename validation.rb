f3150181d9a0261b2d76acfb811116c2525df0c3
Ivan Reshetnyak <resh0607@gmail.com>
2020-01-15 16:12:05 +0300

    Devide Validation module to instance and class methods

--- validation.rb
+++ validation.rb
@@ -1,14 +1,22 @@
 module Validation
-  def validate(name, options = {})
-    define_method(:validate!) do
-      attr = instance_variable_get("@#{name}".to_sym)
-      raise 'No attribute!' if options[:presence] && !attr
-      raise 'Wrong attribute format!' if options[:format] && attr.to_s !~ options[:format]
-      raise 'Wrong type!' if options[:type] && !attr.is_a?(options[:type])
-      true
+  def self.included(base)
+    base.extend ClassMethods
+    base.send :include, InstanceMethods
+  end
+
+  module ClassMethods
+    def validate(name, options = {})
+      define_method(:validate!) do
+        attr = instance_variable_get("@#{name}".to_sym)
+        raise 'No attribute!' if options[:presence] && !attr
+        raise 'Wrong attribute format!' if options[:format] && attr.to_s !~ options[:format]
+        raise 'Wrong type!' if options[:type] && !attr.is_a?(options[:type])
+        true
+      end
     end
   end
 
-  define_method(:valid?) { validate! rescue false }
-  
+  module InstanceMethods
+    define_method(:valid?) { validate! rescue false }
+  end 
 end
