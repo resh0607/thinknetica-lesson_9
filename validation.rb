2e0c7f0fe542d4f638cb6a284f291a79d7bb779d
Ivan Reshetnyak <resh0607@gmail.com>
2020-01-16 10:49:15 +0300

    Minor fix in Validation module

--- validation.rb
+++ validation.rb
@@ -8,7 +8,7 @@ module Validation
     def validate(name, options = {})
       define_method(:validate!) do
         attr = instance_variable_get("@#{name}".to_sym)
-        raise 'No attribute!' if options[:presence] && !attr
+        raise 'No attribute!' if options[:presence] && !attr || attr == ""
         raise 'Wrong attribute format!' if options[:format] && attr.to_s !~ options[:format]
         raise 'Wrong type!' if options[:type] && !attr.is_a?(options[:type])
         true
